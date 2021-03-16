853395b800f313dbc2cc14507b2a71eacddeea72
박진모 <parkjinmo@bagjinmoui-MacBookPro.local>
2021-03-16 23:28:22 +0900

    javascript 다시

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 ## Vailla javascript
+- [X] 계산기
+- []
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,9 +1,26 @@
-const btn = document.querySelectorAll('.number');
+const btn = document.querySelectorAll('.btn');
 
 function handler(){
-    btn[0].addEventListener('click',btnIndex);
+    btn.forEach(btn=>{btn.addEventListener('click',btnIndex)})
 }
-function btnIndex(num){
-    
+
+
+function btnIndex(){
+    let input = document.querySelector('.input');
+    let inputVal = input.value;
+    let result = document.querySelector('.result');
+
+    if(this.dataset.type === 'number'){
+        if(inputVal==='0'){
+            input.value=this.innerText;
+            inputVal += this.innerText;
+        }else{
+            input.value+=this.innerText;
+            inputVal += this.innerText;
+        }
+        console.log(inputVal)
+    }else if(this.dataset.type !=="number"){
+        input.value+=this.innerText;
+    }
 }
 handler()
