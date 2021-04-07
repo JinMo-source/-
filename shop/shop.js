faef52e5d0e0a0b18f227ca03ccf27f011413f55
JinMo-source <qkrwslah123@naver.com>
2021-04-07 23:54:07 +0900

    수정부분 작업

--- shop/shop.js
+++ shop/shop.js
@@ -1,5 +1,7 @@
 //model
 let list = [];
+let fixItem = [];
+
 
 function Item(name,price,img){
     this.name=name;
@@ -11,18 +13,18 @@ function add(name,price,img){
     let item = new Item(name,price,img);
 
     list.push(item);
-    view(list);
 }
 
 function remove(removeIndex){
     list.splice(removeIndex,1);
-    view();
 }
 
 function fix(fixIndex){
-    console.log(fixIndex);
-}
+    const fixData = list[fixIndex];
 
+    fixItem.push(fixData);
+}
+console.log(fix.fixItem);
 // controls
 function addItem(e){
     e.preventDefault();
@@ -42,6 +44,7 @@ function addItem(e){
     inputImg.value = "";
     previewImage.src = 'http://placehold.it/100x100';
 
+    view(list);
 }
 
 function readImage(input) {
@@ -50,35 +53,38 @@ function readImage(input) {
         // 이미지 파일인지 검사 (생략)
         // FileReader 인스턴스 생성
         const reader = new FileReader()
+
         // 이미지가 로드가 된 경우
-        reader.onload = e => {
-            
-            previewImage.src = e.target.result
+        reader.onload = e => { 
+            previewImage.src = e.target.result;
         }
+
         // reader가 이미지 읽도록 하기
         const readerImg = reader.readAsDataURL(input.files[0])
     }
 }
 
-function removeItem(e){
-    if(e.target.tagName !== "INPUT") return;
+function buttonItem(e){
+    if(e.target.tagName !== "BUTTON") return;
 
-    const removeIndex = e.target.id;
-    remove(removeIndex);
+    const buttonIndex = e.target.id;
+    if(e.target.className === "fix"){
+        const fixIndex = buttonIndex.split('-')[1];
+        fix(fixIndex);
 
+        fixView(fixItem);
+
+    } else {
+        remove(buttonIndex);
+    }
+
+    view();
 }
 
-function fixItem(e){
-    if(e.target.tagName !== "BOTTON") return;
-    console.log(e);
-    const fixId = e.target.id;
-    const fixIndex = fixId.split('-');
-    console.log(fixIndex);
-    fix(fixIndex);
-}
 
 // input file에 change 이벤트 부여
 const inputImage = document.getElementById("input-img");
+
 inputImage.addEventListener("change", e => {
     readImage(e.target);
 })
@@ -89,32 +95,45 @@ const inputName = document.querySelector('#input-name');
 const inputPrice = document.querySelector('#input-price');
 const inputImg = document.querySelector("#input-img");
 const previewImage = document.getElementById("preview-image")
+
 form.addEventListener('submit',addItem);
 
-
 const ul = document.querySelector('.shop-ul');        
-ul.addEventListener('click',removeItem);
-ul.addEventListener('click',fixItem);
+ul.addEventListener('click',buttonItem);
+
 // views
-
-
 function view(name,price){
 
     ul.innerHTML = '';
 
-    list.forEach((el,ind)=>
-    {
+    list.forEach((el,ind)=>{
       const li =
       `<li id=list-${ind}" class = "shop-list">
             <p>${el.name}</p>
             <p>${el.price}</p>
             <img src="${el.img}" alt=${el.name} /> 
-            <input type = "checkbox"id="${ind}" class="btn" />
-            <button id="button-${ind}" class="fix">수정</botton>
+            <button id="${ind}" class="btn">삭제</button>
+            <button id="button-${ind}" class="fix">수정</button>
         </li>`
 
         ul.insertAdjacentHTML('afterbegin', li);
-    })
-       
+    })      
 }
 
+function fixView(name){
+
+    const fixDiv = document.querySelector('.list-Fix');
+
+    fixItem.forEach((el,ind)=>{
+        const fixLi = 
+        `<form id="list-fix__form" name = "fix-form">
+            <input type="text" name="name" id = "fix-name" value = "${el.name}" required autofocus/>
+            <input type="text" name="price" id = "fix-input-price" value = "${el.price}"/>
+            <img src= "${el.img}" id = "preview-image" />
+            <input type="file" name="image" id = "input-img" accept="image/*" />
+            <input type="submit" value="submit">
+        </form>`
+
+    fixDiv.insertAdjacentHTML('afterbegin', fixLi);
+    })
+}
